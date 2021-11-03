96df02a0b140ee7aef2640c73cb3800ae9d468d2
Kiran Kanda <kkanda@dons.usfca.edu>
2021-11-02 18:58:48 -0700

    forgot to do any commits but I just updated the readme and added a walkthrough video link to it

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# e-commerce
+# E-commerce
+
+## Description
+This application is built to support a back end for an e-commerce website that uses the latest technologies, which allows a company to stay in the competition with other e-commerce companies. It builds upon a working Express.js API, interacts with Sequelize and a MySQL database.
+
+&nbsp;
+
+## Built With
+* Sequelize
+* MySQL2
+* dotenv package
+* JavaScript
+* Express.js API
+
+&nbsp;
+
+
+## Walkthrough Video
+https://watch.screencastify.com/v/Z74LwZmr1NzPPYhIIBKd
+
+
+&nbsp;
+
+
+## Contribution Guidelines
+
+Made by Winter Kanda 
+
+@2021 Copyright
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,106 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  // be sure to include its associated Product data
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  // be sure to include its associated Product data
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'Tag not found' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData[0]) {
+        res.status(404).json({ message: 'Tag not found' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'Tag not found' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+    
+});
+
+module.exports = router;
